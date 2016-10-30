9824266141b9f5256a5c316945fe705f094e6f56
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-10-30 22:13:08 +0700

    Add AndroidAsync library, bootstrap basic call to /sync API

--- app/src/main/java/info/nskgortrans/maps/MainActivity.java
+++ app/src/main/java/info/nskgortrans/maps/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,16 +18,25 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.koushikdutta.async.ByteBufferList;
+import com.koushikdutta.async.DataEmitter;
+import com.koushikdutta.async.callback.DataCallback;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.AsyncHttpRequest;
+import com.koushikdutta.async.http.AsyncHttpResponse;
+import com.koushikdutta.async.http.callback.HttpConnectCallback;
+
+import org.json.JSONObject;
 
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnCameraIdleListener
 {
-  private int notGrantedPermissions = 4;
-  private int LOCATION_PERMISSION_GRANTED = 10;
-  private int INTERNET_PERMISSION_GRANTED = 11;
-  private int STORAGE_PERMISSION_GRANTED  = 12;
-  private int NETWORK_PERMISSION_GRANTED  = 13;
+  private int notGrantedPermissions = 2;
+  private final int LOCATION_PERMISSION_GRANTED = 10;
+  private final int INTERNET_PERMISSION_GRANTED = 11;
+  private final int STORAGE_PERMISSION_GRANTED  = 12;
+  private final int NETWORK_PERMISSION_GRANTED  = 13;
 
   private SharedPreferences pref;
   private GoogleMap map;
@@ -37,17 +47,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     super.onCreate(savedInstanceState);
 
     // make sure all permissions granted
-    if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-          PackageManager.PERMISSION_GRANTED
-    )
-    {
-      ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-              LOCATION_PERMISSION_GRANTED);
-    }
-    else
-    {
-      notGrantedPermissions--;
-    }
+//    if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+//          PackageManager.PERMISSION_GRANTED
+//    )
+//    {
+//      ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//              LOCATION_PERMISSION_GRANTED);
+//    }
+//    else
+//    {
+//      notGrantedPermissions--;
+//    }
     if ( ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) !=
             PackageManager.PERMISSION_GRANTED
             )
@@ -57,19 +67,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
     else
     {
-      notGrantedPermissions--;
+      performSync(0, 0, 0);
     }
-    if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED
-            )
-    {
-      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-              STORAGE_PERMISSION_GRANTED);
-    }
-    else
-    {
-      notGrantedPermissions--;
-    }
+//    if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+//            PackageManager.PERMISSION_GRANTED
+//            )
+//    {
+//      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//              STORAGE_PERMISSION_GRANTED);
+//    }
+//    else
+//    {
+//      notGrantedPermissions--;
+//    }
     if ( ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
             PackageManager.PERMISSION_GRANTED
             )
@@ -88,6 +98,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     mapFragment.getMapAsync(this);
 
     pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+  }
+
+  private void performSync(long routesTimestamp, long trassesTimestamp, long stopsTimestamp)
+  {
+    String syncUrl = "https://maps.nskgortrans.info/sync?" +
+            "routestimestamp=" + routesTimestamp +
+            "&trassestimestamp=" + trassesTimestamp +
+            "&stopstimestamp=" + stopsTimestamp;
+
+    System.out.println(syncUrl);
+
+    AsyncHttpClient.getDefaultInstance().execute(syncUrl, new HttpConnectCallback()
+    {
+      @Override
+      public void onConnectCompleted(Exception ex, AsyncHttpResponse response)
+      {
+        if (ex != null)
+        {
+          ex.printStackTrace();
+          return;
+        }
+
+        AsyncHttpRequest request = response.getRequest();
+        request.setTimeout(5000);
+        AsyncHttpClient.getDefaultInstance().executeJSONObject(request, new AsyncHttpClient.JSONObjectCallback()
+        {
+          @Override
+          public void onCompleted(Exception e, AsyncHttpResponse source, JSONObject result)
+          {
+            if (e != null)
+            {
+              e.printStackTrace();
+              return;
+            }
+
+            System.out.println("I got a JSONObject: " + result);
+          }
+        });
+      }
+    });
   }
 
   @Override
@@ -119,5 +169,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     LatLng start = new LatLng(lat, lng);
     CameraPosition target = CameraPosition.builder().target(start).zoom(zoom).build();
     _map.moveCamera(CameraUpdateFactory.newCameraPosition(target) );
+  }
+
+  // permissions handling
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+                                         String permissions[], int[] grantResults) {
+    switch (requestCode) {
+      case INTERNET_PERMISSION_GRANTED:
+      {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+          performSync(0, 0, 0);
+        }
+        else
+        {
+          // do nothing and let it crash
+        }
+        return;
+      }
+
+      // other 'case' lines to check for other
+      // permissions this app might request
+    }
   }
 }
