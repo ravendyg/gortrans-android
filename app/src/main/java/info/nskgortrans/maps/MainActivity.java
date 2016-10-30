9f37bb2cd5b4c1a033f712dc1fd493269f8050dc
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-10-30 18:00:07 +0700

    Make map remember it's position

--- app/src/main/java/info/nskgortrans/maps/MainActivity.java
+++ app/src/main/java/info/nskgortrans/maps/MainActivity.java
@@ -13,10 +13,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnCameraIdleListener
 {
 
-  private SharedPreferences _pref;
+  private SharedPreferences pref;
   private GoogleMap map;
 
   @Override
@@ -28,23 +30,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_container);
     mapFragment.getMapAsync(this);
 
-    _pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+    pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
   }
 
-//  @Override
-//  public void onCameraIdle()
-//  {
-//    // set coords
-//  }
+  @Override
+  public void onCameraIdle()
+  {
+    CameraPosition target = map.getCameraPosition();
+    float lat = (float) target.target.latitude;
+    float lng = (float) target.target.longitude;
+    int zoom = Math.round(target.zoom);
+
+    SharedPreferences.Editor editor = pref.edit();
+    editor.putFloat( getString(R.string.pref_lat), lat );
+    editor.putFloat( getString(R.string.pref_lng), lng );
+    editor.putInt( getString(R.string.pref_zoom), zoom );
+    editor.commit();
+  }
 
   @Override
   public void onMapReady(GoogleMap _map)
   {
     map = _map;
 
-    float lat = _pref.getFloat( getString(R.string.pref_lat), (float)54.908593335436926 );
-    float lng = _pref.getFloat( getString(R.string.pref_lng), (float)83.0291748046875 );
-    int zoom =  _pref.getInt( getString(R.string.pref_zoom), 10 );
+    map.setOnCameraIdleListener(this);
+
+    float lat = pref.getFloat( getString(R.string.pref_lat), (float)54.908593335436926 );
+    float lng = pref.getFloat( getString(R.string.pref_lng), (float)83.0291748046875 );
+    int zoom =  pref.getInt( getString(R.string.pref_zoom), 10 );
 
     LatLng start = new LatLng(lat, lng);
     CameraPosition target = CameraPosition.builder().target(start).zoom(zoom).build();
