3b5320f7bc4709ca5024680e7fc74ec721370e8b
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-01-22 07:13:09 +0700

    Remove google malware

--- app/src/main/java/info/nskgortrans/maps/MainActivity.java
+++ app/src/main/java/info/nskgortrans/maps/MainActivity.java
@@ -25,11 +25,6 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -79,11 +74,7 @@ public class MainActivity extends AppCompatActivity {
   private boolean waysLoaded;
 
   private Dialog searchBusDialog = null;
-  /**
-   * ATTENTION: This was auto-generated to implement the App Indexing API.
-   * See https://g.co/AppIndexing/AndroidStudio for more information.
-   */
-  private GoogleApiClient client;
+
 
 //  private SocketIO socket;
 
@@ -159,10 +150,6 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //      }
 //    );
-
-    // ATTENTION: This was auto-generated to implement the App Indexing API.
-    // See https://g.co/AppIndexing/AndroidStudio for more information.
-//    client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
   }
 
   private void loadWayGrous()
@@ -367,40 +354,15 @@ public class MainActivity extends AppCompatActivity {
     return false;
   }
 
-  /**
-   * ATTENTION: This was auto-generated to implement the App Indexing API.
-   * See https://g.co/AppIndexing/AndroidStudio for more information.
-   */
-  public Action getIndexApiAction() {
-    Thing object = new Thing.Builder()
-            .setName("Main Page") // TODO: Define a title for the content shown.
-            // TODO: Make sure this auto-generated URL is correct.
-            .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-            .build();
-    return new Action.Builder(Action.TYPE_VIEW)
-            .setObject(object)
-            .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-            .build();
-  }
 
   @Override
   public void onStart() {
     super.onStart();
-
-//    // ATTENTION: This was auto-generated to implement the App Indexing API.
-//    // See https://g.co/AppIndexing/AndroidStudio for more information.
-//    client.connect();
-//    AppIndex.AppIndexApi.start(client, getIndexApiAction());
   }
 
   @Override
   public void onStop() {
     super.onStop();
-
-//    // ATTENTION: This was auto-generated to implement the App Indexing API.
-//    // See https://g.co/AppIndexing/AndroidStudio for more information.
-//    AppIndex.AppIndexApi.end(client, getIndexApiAction());
-//    client.disconnect();
   }
 /*
   private class SyncData extends AsyncTask<URL, Void, String> {
